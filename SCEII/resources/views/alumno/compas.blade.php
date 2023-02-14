9780d59e3d439666195534a342c3c18e50a311f2
labmanu <labmanitc@gmail.com>
2023-02-14 20:34:29 +0000

    Recuperar

--- SCEII/resources/views/alumno/compas.blade.php
+++ SCEII/resources/views/alumno/compas.blade.php
@@ -3,34 +3,22 @@
     @extends('templates.alumno')
     @section('title', 'Compañeros de Laboratorio')
 
-    <!-- -->
-    <script >
-        var token = '<?php echo $_SESSION["data"]->token; ?>';
-    </script>
-    <!-- -->
-
     @section('content')
-
         <!-- Contenido -->
         <div class="home">
-            <h3 class="titulo text-center">
+            <h3 class="titulo text-center mt-2">
                 {{$_SESSION["laboratorio"]->nombre}}
             </h3>
-
+            <br>
             <div class="conp">
                 <h5>Compañeros</h5>
-
                 @foreach ($_SESSION["compas"] as $compa)
                     <div class="comp py-1">
-                        <img class="img-compas" src="{{$compa->foto}}" alt="img" />
-                        {{$compa->nombre}}
+                        <img class="img-compas" src="{{$compa->foto}}" alt="img" /> 
+                         {{$compa->nombre}}
                     </div>
-                    {{-- <br> --}}
                 @endforeach
-                
             </div>
-            
-            {{-- {{var_dump($_SESSION["compas"])}} --}}
         </div>
     @endsection
 
